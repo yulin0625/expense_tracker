a7889ceae08e7f0446f5e31e4c2dbf39786765cd
yulin0625 <yulin90625@gmail.com>
2024-03-08 00:50:12 +0800

    first commit

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime(2024, 2, 20, 21, 40),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter ExpenseTracker'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          const Text('The chart'),
+          Expanded(
+            // 因為外面是Column, 裡面是ListViewer, flutter會不知道List的寬度, 因此需用Expanded包住List
+            child: ExpensesList(expense: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_itme.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({super.key, required this.expense});
+
+  final List<Expense> expense;
+
+  @override
+  Widget build(BuildContext context) {
+    // 使用ListView動態產生需要的Widget
+    return ListView.builder(
+      itemCount: expense.length,
+      itemBuilder: (ctx, index) => ExpensesItem(expense[index]),
+    );
+  }
+}
