340e359f0e9d693e705e494ea19bf7dbb4efaf89
yulin0625 <yulin90625@gmail.com>
2024-03-24 19:46:11 +0800

    Saving New Expenses

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -5,7 +5,8 @@ import 'package:intl/intl.dart';
 final formatter = DateFormat.yMd();
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({super.key, required this.onAddExpense});
+  final void Function(Expense expense) onAddExpense;
 
   @override
   State<NewExpense> createState() {
@@ -61,6 +62,14 @@ class _NewExpenseState extends State<NewExpense> {
       );
       return;
     }
+    widget.onAddExpense(
+      Expense(
+        title: _titleController.text,
+        amount: enteredAmount,
+        date: _selectedDate!,
+        category: _selectedCategory,
+      ),
+    );
   }
 
   /// 在沒有使用到 TextEditingController 時要使用 dispose 將記憶體釋放
